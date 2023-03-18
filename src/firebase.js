0de308a7234d9972b972c0c9541ec652239c3acc
patrick-steve <75305373+patrick-steve@users.noreply.github.com>
2023-03-18 16:25:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import firebase from 'firebase/compat/app'
 import "firebase/compat/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDq-oAMUsyquA3PC4gW76l8osrOeQxHkYc",
-    authDomain: "videochat-62d9a.firebaseapp.com",
-    projectId: "videochat-62d9a",
-    storageBucket: "videochat-62d9a.appspot.com",
-    messagingSenderId: "641858529403",
-    appId: "1:641858529403:web:dbcd574ef51d98a23d1ade",
-    measurementId: "G-31TCH6P3VD"
+    apiKey: "***",
+    authDomain: "***",
+    projectId: "***",
+    storageBucket: "***",
+    messagingSenderId: "***",
+    appId: "***",
+    measurementId: "***"
 };
 
 if(!firebase.apps.length) {
